4f8233b3915fade712e0af96df289b3c5b58c624
Lucas Coelho <l_coelhox@hotmail.com>
2022-05-13 16:03:51 -0300

    Segundo dia em JS

---
+++ Exercicios-Trybe/fundamentos/Scrips-tests/scrips.js
@@ -0,0 +1,20 @@
+
+//let lista = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+//for(let index = 0;  index < lista.length; index += 1) {
+//    console.log(lista[index]);
+//}
+
+
+//let cars = ["Saab", "Volvo", "BMW"];
+
+//for (let index = 0; index < cars.length; index += 1) {
+  //console.log(cars[index]);
+//}
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let name of names){
+    console.log(name);
+}
---
+++ Exercicios-Trybe/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,106 @@
+//const a = 5
+//const b = 7
+//const c = 9
+
+//let re = a % b
+
+//if (a > b, c){
+  //  console.log(a)
+
+//}else if (b > a, c){
+//    console.log(b)
+//}else{
+//    console.log(c)
+//}
+
+//const valor = -5
+
+//if (valor > 0){
+  //  console.log('positivo')
+//} else if (valor === 0) {
+  //  console.log('zero')
+//} else if (valor < 0) {
+  //  console.log('negativo')
+//}
+
+
+//const a1 = 60
+//const a2 = 60
+//const a3 = 60
+//const angulo = a1 + a2 +a3
+
+//if (angulo === 180){
+  //  console.log(true)
+//}
+//else{
+//    console.log(false)
+//}
+
+
+//let p = 'peão'
+//if (p){
+  //  console.log('anda para frente')
+//}
+
+
+
+//let nota 
+//nota = 0
+
+//if (nota >= 90){
+ //   console.log('Nota A')
+//}
+//else if (nota >= 80){
+  //  console.log('Nota B')
+//}
+//else if (nota >= 70){
+   // console.log('Nota C')
+//}
+//else if (nota >= 60){
+  //  console.log('Nota D')
+//}
+//else if (nota >= 50){
+  //  console.log('Nota E')
+//}
+//else if (nota < 50 && nota > 0){
+ //   console.log('Nota F')
+//}
+//else if (nota === 0){
+//    console.log('Erro')
+//}
+
+//let v = 4
+//let num = v  
+
+//if (v / v === 0 ){
+  //  console.log(true)
+//}
+//else{
+  //  console.log(false)
+//}
+
+
+
+//const prod = 100
+//const prodv = 150
+//const prodf = 120
+//const dif = prodv - prodf  
+
+//console.log(dif * 1000)
+
+let salb = 1900
+let inss = 8% salb
+let ir
+let resul
+resul = salb - inss - ir
+
+if (salb <= 1903,98){
+    inss = 8% ir === 0
+    console.log(resul)
+}
+
+
+
+
+
+
