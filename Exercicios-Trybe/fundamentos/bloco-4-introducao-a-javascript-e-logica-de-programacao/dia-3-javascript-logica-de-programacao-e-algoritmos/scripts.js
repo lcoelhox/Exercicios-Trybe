7dc4b20ba88267dcfbf2c39a5be2cec02f121f14
Lucas Coelho <l_coelhox@hotmail.com>
2022-05-16 19:47:38 -0300

    Exercicios do dia 4.3

---
+++ Exercicios-Trybe/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -0,0 +1,32 @@
+
+
+
+//FATORIAL DE 10
+// let numF = 1
+// let num  = 10
+
+// for (let index = 1; index <= num; index += 1){
+//     numF = numF * index
+//     console.log(numF)
+// }
+
+
+let wordR = ''
+let word = 'tryber';
+for ( let index = 0; index < word.length; index += 1){
+    wordR = wordR + word[index]
+    console.log(wordR)
+    console.log(word.reverse)
+}
+console.log(word.split(""))
+console.log(reverse(word))
+
+
+
+let maior = ""
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+for (index = 0; index <= array; index += 1){
+    if (array.length[index] > maior.length)
+    maior = array[index]
+}
+console.log(maior)
