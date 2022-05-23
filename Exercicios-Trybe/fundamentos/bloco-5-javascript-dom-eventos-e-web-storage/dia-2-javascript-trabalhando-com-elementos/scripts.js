f823e4ada2ebac42f561d9f43b1e5cff5bd6baf8
Lucas Coelho <l_coelhox@hotmail.com>
2022-05-23 13:59:44 -0300

    Exercicios 5.2

--- Exercicios-Trybe/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/scripts.js
+++ Exercicios-Trybe/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/scripts.js
@@ -1,29 +1,21 @@
 let father = document.getElementById('pai');
-let body = document.getElementById('body')
-let h1 = document.createElement('h1');
+let body = document.getElementById('body');
 
-father.appendChild(body);
-function createElement(father);
-function createElement(body);
+let h1 = document.createElement('h1');
 body.appendChild(h1);
-function createElement(h1);
-h1.innerText = 'Exercicio 5.2';
+h1.innerText = 'Exercicio 5.2 - JavaScript DOM';
 
 let main = document.createElement('main');
 body.appendChild(main);
-main.className('main-content')
-function createElement(main);
-
+main.className = 'main-content';
 
 let sectionC = document.createElement('section');
 main.appendChild(sectionC);
-sectionC.className('center-conttent')
-function createElement(sectionC);
+sectionC.className = 'center-conttent';
 
 let p = document.createElement('p');
 sectionC.appendChild(p);
-p.innerText = 'Esse é apenas um paragrafo de test, fique tranquilo e mantenha a tranquilidade'
-function createElement(p);
+p.innerText = 'Esse é apenas um paragrafo de teste, fique tranquilo e mantenha a tranquilidade';
 
 let sectionL = document.createElement('section');
 main.appendChild(sectionL);
@@ -34,5 +26,14 @@ main.appendChild(sectionR);
 let imagem = document.createElement('img');
 sectionL.appendChild(imagem);
 imagem.src('https://picsum.photos/200');
-imagem.className('small.image');
+imagem.className = 'small.image';
 
+let ul1 = Document.createElement('ul');
+let lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for ( index = 0; index < lista.length;index += 1) {
+    let listaN = lista[index];
+    ul1.innerText = lista
+    ul1.appendChild(listaN) 
+}
+sectionR.appendChild(ul1);
