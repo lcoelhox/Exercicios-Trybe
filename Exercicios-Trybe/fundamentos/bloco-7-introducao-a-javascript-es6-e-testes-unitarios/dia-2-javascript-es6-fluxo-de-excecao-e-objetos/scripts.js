50c24aab2faa985edc45b2ab1bf9055e97986d3b
Lucas Coelho <l_coelhox@hotmail.com>
2022-06-09 19:24:20 -0300

    Exercicios 7.1(não terminado)

---
+++ Exercicios-Trybe/fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/scripts.js
@@ -0,0 +1,53 @@
+// const order = {
+//     name: 'Rafael Andrade',
+//     phoneNumber: '11-98763-1416',
+//     address: {
+//       street: 'Rua das Flores',
+//       number: '389',
+//       apartment: '701',
+//     },
+//     order: {
+//       pizza: {
+//         marguerita: {
+//           amount: 1,
+//           price: 25,
+//         },
+//         pepperoni: {
+//           amount: 1,
+//           price: 20,
+//         }
+//       },
+//       drinks: {
+//         coke: {
+//           type: 'Coca-Cola Zero',
+//           price: 10,
+//           amount: 1,
+//         }
+//       },
+//       delivery: {
+//         deliveryPerson: 'Ana Silveira',
+//         price: 5,
+//       }
+//     },
+//     payment: {
+//       total: 60,
+//     },
+//   };
+  
+//   const customerInfo = (order) => {
+//     // Adicione abaixo as informações necessárias.
+//   const nome1 = Object.values.delivery
+//   console.log(nome1);
+// //   const nome2 = 
+// //   const telefone =
+// //   const nun =
+//   }
+  
+//   customerInfo(order);
+  
+//   const orderModifier = (order) => {
+//     // Adicione abaixo as informações necessárias.
+  
+//   }
+  
+//   orderModifier(order);
